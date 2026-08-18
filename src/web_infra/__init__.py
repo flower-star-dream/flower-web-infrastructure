bc4a4336aa2a1b7124a3974c348989634cbe678c
花海 <1378281299@qq.com>
2026-08-18 19:20:13 +0800

    chore: 同步版本号 0.1.0-dev8

--- src/web_infra/__init__.py
+++ src/web_infra/__init__.py
@@ -354,7 +354,7 @@ from web_infra.infra.utils import (
 )
 from web_infra.core.application import Application, create_app
 
-__version__ = "0.1.0-dev7"
+__version__ = "0.1.0-dev8"
 
 __all__ = [
     # 响应与错误码
